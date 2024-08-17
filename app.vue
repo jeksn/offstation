371125d0a81361a1ae1c8c6aecbb776b29dca2c1
Johan <johan@jeksn.me>
2024-08-17 10:07:51 +0200

    add scrolling marquee banner

--- app.vue
+++ app.vue
@@ -1,18 +1,18 @@
 <template>
-  <main class="min-h-screen bg-neutral-900 text-white">
+  <main class="min-h-screen text-white bg-neutral-900">
     <Nav />
     <Hero />
-    <div class="max-w-screen-lg mx-auto flex flex-col gap-6 pt-6 pb-24">
-      <p class="font-body text-lg">
+    <div class="flex flex-col max-w-screen-lg gap-6 py-24 mx-auto">
+      <p class="text-lg font-body">
         Work in progress!
       </p>
-      <p class="font-body text-lg">
+      <p class="text-lg font-body">
         Sunt ex tempor officia id. Duis excepteur amet Lorem dolore. Aute reprehenderit ut consequat elit pariatur dolor nulla incididunt ut ad anim culpa consectetur ea ipsum. Dolore aute est qui. Do aute nulla elit duis commodo amet qui reprehenderit aliquip consectetur consectetur amet aliquip elit. Est aliqua officia non incididunt excepteur duis exercitation. Labore ullamco mollit do deserunt et ad ex excepteur Lorem. Qui nisi esse dolore commodo pariatur ut irure esse sint amet cupidatat.
       </p>
-      <p class="font-body text-lg">
+      <p class="text-lg font-body">
         Sunt ex tempor officia id. Duis excepteur amet Lorem dolore. Aute reprehenderit ut consequat elit pariatur dolor nulla incididunt ut ad anim culpa consectetur ea ipsum. Dolore aute est qui. Do aute nulla elit duis commodo amet qui reprehenderit aliquip consectetur consectetur amet aliquip elit. Est aliqua officia non incididunt excepteur duis exercitation. Labore ullamco mollit do deserunt et ad ex excepteur Lorem. Qui nisi esse dolore commodo pariatur ut irure esse sint amet cupidatat.
       </p>
-      <p class="font-body text-lg">
+      <p class="text-lg font-body">
         Sunt ex tempor officia id. Duis excepteur amet Lorem dolore. Aute reprehenderit ut consequat elit pariatur dolor nulla incididunt ut ad anim culpa consectetur ea ipsum. Dolore aute est qui. Do aute nulla elit duis commodo amet qui reprehenderit aliquip consectetur consectetur amet aliquip elit. Est aliqua officia non incididunt excepteur duis exercitation. Labore ullamco mollit do deserunt et ad ex excepteur Lorem. Qui nisi esse dolore commodo pariatur ut irure esse sint amet cupidatat.
       </p>
     </div>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,14 +1,30 @@
+
 <template>
     <div class="relative">
         <div class="max-w-screen-lg h-[600px] flex-col gap-4 justify-center flex mx-auto px-4 z-20 relative">
-            <div class="flex flex-col gap-6 w-full md:w-5/6">
-                <h1 class="text-3xl md:text-6xl font-heading font-bold">Effective solutions that work for you and your brand</h1>
+            <div class="flex flex-col w-full gap-6 md:w-5/6">
+                <h1 class="text-3xl font-bold md:text-6xl font-heading">Effective solutions that work for you and your brand</h1>
                 <h2 class="text-xl md:text-2xl font-body">Offstation is a remote agency specializing in web development and branding</h2>
             </div>
         </div>
         <div class="absolute bottom-0 left-0 w-full h-[600px] z-10">
-            <img src="/public/assets/images/space-bg.jpeg" alt="" class="h-full w-full">
+            <img src="/public/assets/images/space-bg.jpeg" alt="" class="w-full h-full">
             <div class="absolute bottom-0 left-0 w-full h-[600px] bg-gradient-to-t from-neutral-900"></div>
+        </div>
+    </div>
+    <div class="relative flex overflow-x-hidden text-black bg-white">
+        <div class="py-12 text-3xl animate-marquee font-heading">
+            <span class="mx-8">Branding</span>
+            <span class="mx-8">Marketing strategy</span>
+            <span class="mx-8">Web development</span>
+            <span class="mx-8">Logo designs</span>
+        </div>
+
+        <div class="absolute top-0 py-12 text-3xl animate-marquee2 font-heading">
+            <span class="mx-8">Branding</span>
+            <span class="mx-8">Marketing strategy</span>
+            <span class="mx-8">Web development</span>
+            <span class="mx-8">Logo designs</span>
         </div>
     </div>
 </template>
